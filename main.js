e757938e63d1e1399a54474bc565bc99ed01bd59
César Guerra <warderer@gmail.com>
2020-07-18 08:43:41 -0500

    send button clic refactor and show initial cards content

--- main.js
+++ main.js
@@ -13,37 +13,43 @@
         return results;
     }
 
+    function showFilteredCars(){
+            let make = document.getElementById("makeSelect").value;
+            let year = document.getElementById("yearSelect").value;
+            let budget = parseInt(document.getElementById("budget").value);
+            let transmision = document.getElementById("transmisionSelect").value;
+            let filtro = filterCars(make, year, budget, transmision);
+
+            // Preparo el elemento donde se mostrará la información del filtro de carros
+            var listaCarros = document.getElementById('carsList-container');
+            listaCarros.innerHTML="";
+            var tmpHTML="";
+
+            // Recorrer arreglo de carros filtrados
+            for (var index = 0; index < filtro.length; index++) {
+                var unAuto = filtro[index];
+                var fMake = unAuto.make;
+                var fModel = unAuto.model;
+                var fTransmision = unAuto.transmision;
+                var fColors = unAuto.colors;
+                var fPrice = unAuto.price;
+                var fYear = unAuto.year;    
+                var fBodystyles = unAuto.body_styles;
+
+                //Creo una tarjeta y la guardo dentro del HTML a mostrar
+                tmpHTML = tmpHTML + createCarCard(fMake, fModel, fTransmision, fColors, fPrice, fYear, fBodystyles);
+            }
+            //Coloco todo el contenido generado en el HTML
+            listaCarros.innerHTML=tmpHTML;
+    }
+
     document.getElementById("sendButton").addEventListener("click", function(){
-        let make = document.getElementById("makeSelect").value;
-        let year = document.getElementById("yearSelect").value;
-        let budget = parseInt(document.getElementById("budget").value);
-        let transmision = document.getElementById("transmisionSelect").value;
-
-        var filtro = filterCars(make, year, budget, transmision);
-        // Preparo el elemento donde se mostrará la información del filtro de carros
-        var listaCarros = document.getElementById('carsList-container');
-        listaCarros.innerHTML="";
-        var tmpHTML="";
-
-        // Recorrer arreglo de carros filtrados
-        for (var index = 0; index < filtro.length; index++) {
-            var unAuto = filtro[index];
-            var fMake = unAuto.make;
-            var fModel = unAuto.model;
-            var fTransmision = unAuto.transmision;
-            var fColors = unAuto.colors;
-            var fPrice = unAuto.price;
-            var fYear = unAuto.year;    
-            var fBodystyles = unAuto.body_styles;
-
-            //Creo una tarjeta y la guardo dentro del HTML a mostrar
-            tmpHTML = tmpHTML + createCarCard(fMake, fModel, fTransmision, fColors, fPrice, fYear, fBodystyles);
-        }
-        //Coloco todo el contenido generado en el HTML
-        listaCarros.innerHTML=tmpHTML;
-        
-        event.preventDefault()
-
+        showFilteredCars();
+        event.preventDefault();
     });
+
+    /* Cargamos una preselección de contenido al entrar a la página*/
+    filterCars('Chevrolet',2020,60000,'auto');
+    showFilteredCars();
 
 })();
